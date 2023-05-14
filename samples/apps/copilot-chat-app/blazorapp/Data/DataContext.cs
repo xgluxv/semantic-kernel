f90d2efae05ac974e18edffe1dfc4dd25675cfa2
lu xu <xgluxv@hotmail.com>
2023-05-15 00:27:28 +0800

    chang only to net7

---
+++ samples/apps/copilot-chat-app/blazorapp/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CopilotChatBlazorServerClient.Data;
+
+public class DataContext:IdentityDbContext
+{
+    public DataContext(DbContextOptions options):base(options)
+    {
+        
+    }
+}
