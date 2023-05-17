0cc7c2c83e15151c1ff253cbdf6638a8cb8527e2
lu xu <xgluxv@hotmail.com>
2023-05-17 14:56:39 +0800

    111#

--- samples/apps/copilot-chat-app/blazorapp/Program.cs
+++ samples/apps/copilot-chat-app/blazorapp/Program.cs
@@ -1,6 +1,8 @@
-// Copyright (c) Microsoft. All rights reserved.
+﻿// Copyright (c) Microsoft. All rights reserved.
 
 using CopilotChatBlazorServerClient.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Fast.Components.FluentUI;
 
 WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
@@ -8,12 +10,30 @@ WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+var cs=builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlite(cs));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+    {
+        options.Password.RequireDigit = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredLength = 5;
+        options.SignIn.RequireConfirmedEmail = false;
+        options.SignIn.RequireConfirmedAccount = false;
+        options.SignIn.RequireConfirmedPhoneNumber = false;
+    }
+)
+    .AddEntityFrameworkStores<DataContext>();
+
 builder.Services.AddHttpClient();
 
-builder.Services.AddFluentUIComponents();
+//builder.Services.AddFluentUIComponents();
 //When using icons and/or emoji replace the line above with the code below
-//LibraryConfiguration config = new(ConfigurationGenerator.GetIconConfiguration(), ConfigurationGenerator.GetEmojiConfiguration());
-//builder.Services.AddFluentUIComponents(config);
+LibraryConfiguration config = new(ConfigurationGenerator.GetIconConfiguration(), ConfigurationGenerator.GetEmojiConfiguration());
+builder.Services.AddFluentUIComponents(config);
 
 builder.Services.AddSingleton<WeatherForecastService>();
 
@@ -32,6 +52,10 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
